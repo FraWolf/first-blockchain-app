65a27027c8d59c0b032a121e72770af0f7d6a9dc
Fra <frawolf@outlook.it>
2020-12-17 12:09:40 +0100

    Fixed typo

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract TodoList {
 
     // Uint => unsigned int
-    // public keyboard, to use the variable like a function (ex: this.taskCount() = 0)
+    // public key, to use the variable like a function (ex: this.taskCount() = 0)
 
     uint public taskCount = 0;
     
